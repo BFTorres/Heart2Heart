4f08484d1f932cb4c015d645738c9f8db31b4191
BFT <bftorres@protonmail.com>
2026-02-15 22:52:36 +0100

    ci: deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/<Heart2Heart>/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
